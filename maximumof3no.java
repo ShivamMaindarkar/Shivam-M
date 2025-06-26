2e3c9380cedad92065f5d9b39fe4a871d51801d3
ShivamMaindarkar <160278089+ShivamMaindarkar@users.noreply.github.com>
2025-06-26 13:43:54 +0530

    Add files via upload

---
+++ maximumof3no.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class maximumof3no {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int a = sc.nextInt();
+
+        System.out.print("Enter second number: ");
+        int b = sc.nextInt();
+
+        System.out.print("Enter third number: ");
+        int c = sc.nextInt();
+
+        int max;
+        if (a >= b && a >= c) {
+            max = a;
+        } else if (b >= a && b >= c) {
+            max = b;
+        } else {
+            max = c;
+        }
+
+        System.out.println("The maximum number is: " + max);
+    }
+}
